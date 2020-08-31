b2fe5788d146f1e60fda9639a050730a5ac6f8d6
Alex <ZimenkovIUAI@gmail.com>
2020-08-31 22:00:32 +0300

    Выполнил задание 1 (ч.1)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+    actors: {},
+    genres:[],
+    privat: false
+};
+
+const lastFilms = {
+	nameFilm1: prompt('Один из последних просмотренных фильмов?', ''),
+	scoreFilm1: prompt('На сколько оцените его?', ''),
+	nameFilm2: prompt('Один из последних просмотренных фильмов?', ''),
+	scoreFilm2: prompt('На сколько оцените его?', '')
+};	
+personalMovieDB.movies[lastFilms.nameFilm1] = lastFilms.scoreFilm1;
+personalMovieDB.movies[lastFilms.nameFilm2] = lastFilms.scoreFilm2;
+
+console.log(personalMovieDB);
