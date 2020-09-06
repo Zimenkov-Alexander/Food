a42d7e08c6a13b69b3208cca2b33b655768a0a58
Alex <ZimenkovIUAI@gmail.com>
2020-09-06 20:25:40 +0300

    Сделал задание 2 (ч.1)

--- js/main.js
+++ js/main.js
@@ -1,77 +1,35 @@
 "use strict";
 
-const personalMovieDB = {
-	count: () => {
-		let number = +prompt('Сколько фильмов вы уже посмотрели?', '');
-	
-		while (number == '' || number == null || isNaN(number)) {
-			number = +prompt('Сколько фильмов вы уже посмотрели?', '');
-		}
-		return number;
-	},
-	movies: () => {
+const ad = document.querySelector('.promo__adv'),
+	  adBlocks= ad.querySelectorAll('img'),
+	  adTitle = ad.querySelector('.promo__adv-title'),
+	  promoBg = document.querySelector('.promo__bg'),
+	  promoGenre = promoBg.querySelector('.promo__genre'),
+	  promoInteractiveItems = document.querySelectorAll('.promo__interactive-item');
 
-		let movies = {};
-	
-		for (let i = 0; i<2; i++){
-			let nameFilm = prompt('Один из последних просмотренных фильмов?', ''),
-				scoreFilm = +prompt('На сколько оцените его?', '');
-		
-			if (nameFilm && scoreFilm && nameFilm != '' && scoreFilm != '' && nameFilm.length < 50){
-				movies[nameFilm] = scoreFilm;
-			} 
-			else{
-				i--;
-			}
-		}
-		return movies;
-	},
-    actors: {},
-    genres: () => {
-		let genreFilms = []; 
-		let genre;
-		for (let i = 0; i < 3; i++){
-			genre = prompt(`Ваш любимый жанр под номером ${i+1}`, '');
-			if (genre && genre != '' && genre.length < 15){
-				genreFilms.push(genre);
-			} 
-			else{
-				i--;
-			}
-		}
-		genreFilms.forEach((key, i) => console.log(`Любимый жанр №${i+1} - это ${key}`));
-		return genreFilms;
-	},
-	privat: false,
-	toggleVisibleMyDB: (privatToggle) => {
-		if (privatToggle){
-			personalMovieDB.privat = false;
-		} else{
-			personalMovieDB.privat = true;
-		}
-	},
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
 };
+movieDB.movies.sort();
 
+// Задание 1
+adBlocks.forEach(item => item.remove());
+adTitle.textContent = '';
 
-const detectPersonalLevel = () => {
-	if (personalMovieDB.count < 10) {
-		alert ("Просмотрено довольно мало фильмов");
-	} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
-		alert ("Вы классический зритель");
-	} else if (personalMovieDB.count >= 30){
-		alert ("Вы киноман");
-	} else{
-		alert ("Произошла ошибка");
-	}
-};
+// Задание 2
+promoGenre.textContent = 'ДРАМА';
 
-// detectPersonalLevel();
+// Задание 3
+promoBg.style.cssText = "background-image: url(../image/bg.jpg)"; 
 
-const showMyDB = (privatToggle) => {
-	if (!privatToggle){
-		console.log(personalMovieDB);
-	}
-};
-showMyDB(personalMovieDB.privat);
-
-
+// Задание 4 и 5
+promoInteractiveItems.forEach((item, i) => (
+	item.textContent = `${i+1}. ${movieDB.movies[i]}.`
+));
